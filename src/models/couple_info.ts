d929de882f89a883b7b3feae5e2906b0fd63c3cc
iamqh <rubik0403@gmail.com>
2024-02-15 22:01:31 +0700

    add images

--- src/models/couple_info.ts
+++ src/models/couple_info.ts
@@ -11,12 +11,14 @@ export const coupleInfos: CoupleInfo[] = [
         name: "Phạm Quang Hoà",
         dadName: "Phạm Quang Điện",
         momName: "Đỗ Thị Hương",
-        address: "Như Quỳnh, Văn Lâm, Hưng Yên"
+        address: "Như Quỳnh, Văn Lâm, Hưng Yên",
+        image: "/assets/images/couple/2.jpg"
     },
     {
         name: "Kiều Trang",
         dadName: "Kiều Đức Thành",
         momName: "Đỗ Thị Thìn",
-        address: "Trưng Trắc, Văn Lâm, Hưng Yên"
+        address: "Trưng Trắc, Văn Lâm, Hưng Yên",
+        image: "/assets/images/couple/1.jpg"
     },
 ]
